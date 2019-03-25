613d9daf6a17eec70b4374132d62152844832f8e
drdynscript <drdynscript@gmail.com>
2019-03-25 18:44:56 +0100

    Update routes in the right order

--- src/server/api/v1/controller/post.controller.js
+++ src/server/api/v1/controller/post.controller.js
@@ -6,13 +6,11 @@ import uuidv4 from 'uuid/v4';
 
 /*
 Import the internal libraries:
-- posts.mock.js
+- * from mocks
 - errorHandler
 */
-import * as mockPosts from '../mocks/posts.mock';
+import * as mockDb from '../mocks';
 import { handleAPIError } from '../utilities';
-
-let posts = mockPosts.posts;
 
 class PostController {
     constructor() {
@@ -21,16 +19,16 @@ class PostController {
 
     // List all the models
     index = (req, res, next) => {
-        if(posts === undefined || posts === null) {
-            return handleAPIError(404, `Type Posts not found!`, next);
+        if(mockDb.posts === undefined || mockDb.posts === null) {
+            return handleAPIError(404, `Type Post not found!`, next);
         }
-        return res.status(200).json(posts);
+        return res.status(200).json(mockDb.posts);
     }
 
     // Show specific model by id
     show  = (req, res, next) => { 
         const id = req.params.id;
-        const item = posts.find((obj) => {
+        const item = mockDb.posts.find((obj) => {
             return obj.id === id;
         });
         if(item === undefined || item === null) {
@@ -54,14 +52,14 @@ class PostController {
             return handleAPIError(400, `Could not create the Post, because the request is bad!`, next);
         }
         post.id = uuidv4();
-        posts.push(post);
+        mockDb.posts.push(post);
         return res.status(201).json(post);
     }
 
     // ViewModel for Edit / Update
     edit = (req, res, next) => {
         const id = req.params.id;
-        const item = posts.find((obj) => {
+        const item = mockDb.posts.find((obj) => {
             return obj.id === id;
         });
         if(item === undefined || item === null) {
@@ -77,32 +75,32 @@ class PostController {
     // Update the model
     update = (req, res, next) => {  
         const id = req.params.id; 
-        const idx = posts.findIndex((obj) => {
+        const idx = mockDb.posts.findIndex((obj) => {
             return obj.id === id;
         });
         if(idx === -1) {
             return handleAPIError(404, `Post with id: ${id} not found!`, next);
         }     
-        const item = posts[idx]
+        const item = mockDb.posts[idx]
         let post = req.body;
         if(post === undefined || post === null || post == {}) {
             return handleAPIError(400, `Could not create the Post, because the request is bad!`, next);
         }
         post.id = item.id;
-        posts[idx] = post;
+        mockDb.posts[idx] = post;
         return res.status(200).json(post);
     }
 
     // Delete / Destroy the model
     destroy = (req, res, next) => {  
         const id = req.params.id; 
-        const item = posts.find((obj) => {
+        const item = mockDb.posts.find((obj) => {
             return obj.id === id;
         });
         if(item === undefined || item === null) {
             return handleAPIError(404, `Post with id: ${id} not found!`, next);
         }
-        posts = posts.filter(obj => obj.id !== id);
+        mockDb.posts = mockDb.posts.filter(obj => obj.id !== id);
         return res.status(200).json({ 'message': `Successful deleted the Post with id: ${id}!`});
     }
 }
--- src/server/api/v1/routes/post.routes.js
+++ src/server/api/v1/routes/post.routes.js
@@ -7,8 +7,8 @@ const postController = new PostController();
 
 const initializeEndpoints = (parentRouter) => {
     parentRouter.get('/posts', postController.index);
-    parentRouter.get('/posts/:id', postController.show);   
     parentRouter.get('/posts/create', postController.create)
+    parentRouter.get('/posts/:id', postController.show);   
     parentRouter.post('/posts', postController.store)
     parentRouter.get('/posts/:id/edit', postController.edit)
     parentRouter.put('/posts/:id', postController.update)
