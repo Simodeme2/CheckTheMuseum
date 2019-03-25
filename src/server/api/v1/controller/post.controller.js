980f8bc7936c2f6e6b9318ac72ab4f67cad37772
drdynscript <drdynscript@gmail.com>
2019-03-25 16:16:42 +0100

    Add more features to post.controller: create, update, delete

--- src/server/api/v1/controller/post.controller.js
+++ src/server/api/v1/controller/post.controller.js
@@ -1,4 +1,10 @@
 /*
+Import the external libraries:
+- uuidv4
+*/
+import uuidv4 from 'uuid/v4';
+
+/*
 Import the internal libraries:
 - posts.mock.js
 - errorHandler
@@ -15,11 +21,14 @@ class PostController {
 
     // List all the models
     index = (req, res, next) => {
+        if(posts === undefined || posts === null) {
+            return handleAPIError(404, `Type Posts not found!`, next);
+        }
         return res.json(posts);
     }
 
     // Show specific model by id
-    show  = (req, res, next) => {
+    show  = (req, res, next) => { 
         const id = req.params.id;
         const item = posts.find((obj) => {
             return obj.id === id;
@@ -32,38 +41,70 @@ class PostController {
 
     // ViewModel for Insert / Create
     create = (req, res, next) => {
-        
-        const item = posts.find((obj) => {
-            return obj.name == id;
-        });
-        throw new Error('Not implemented yet!');
+        const vm = {
+            "categories": []
+        };
+        return res.json(vm);
     }
 
     // Store / Create the new model
     store = (req, res, next) => {        
         let post = req.body;
+        if(post === undefined || post === null) {
+            return handleAPIError(400, `Could not create the Post, because the request is bad!`, next);
+        }
         post.id = uuidv4();
-        return res.json(post);
+        posts.push(post);
+        console.log(posts);
+        return res.status(201).json(post);
     }
 
     // ViewModel for Edit / Update
     edit = (req, res, next) => {
         const id = req.params.id;
-        throw new Error('Not implemented yet!');
+        const item = posts.find((obj) => {
+            return obj.id === id;
+        });
+        if(item === undefined || item === null) {
+            return handleAPIError(404, `Post with id: ${id} not found!`, next);
+        }
+        const vm = {
+            "post": item,
+            "categories": []
+        }
+        return res.status(200).json(vm);
     }
 
     // Update the model
     update = (req, res, next) => {  
-        const id = req.params.id;      
+        const id = req.params.id; 
+        const idx = posts.findIndex((obj) => {
+            return obj.id === id;
+        });
+        if(idx === -1) {
+            return handleAPIError(404, `Post with id: ${id} not found!`, next);
+        }     
+        const item = posts[idx]
         let post = req.body;
+        if(post === undefined || post === null || post == {}) {
+            return handleAPIError(400, `Could not create the Post, because the request is bad!`, next);
+        }
+        post.id = item.id;
+        posts[idx] = post;
         return res.json(post);
     }
 
     // Delete / Destroy the model
     destroy = (req, res, next) => {  
-        const id = req.params.id;      
-        posts = posts.filter(item => item.id !== id);
-        return res.json({ 'message': 'ok '});
+        const id = req.params.id; 
+        const item = posts.find((obj) => {
+            return obj.id === id;
+        });
+        if(item === undefined || item === null) {
+            return handleAPIError(404, `Post with id: ${id} not found!`, next);
+        }
+        posts = posts.filter(obj => obj.id !== id);
+        return res.json({ 'message': `Successful deleted the Post with id: ${id}!`});
     }
 }
 export default PostController;
--- src/server/api/v1/routes/post.routes.js
+++ src/server/api/v1/routes/post.routes.js
@@ -8,5 +8,10 @@ const postController = new PostController();
 const initializeEndpoints = (parentRouter) => {
     parentRouter.get('/posts', postController.index);
     parentRouter.get('/posts/:id', postController.show);
+    parentRouter.get('/posts/create', postController.create)
+    parentRouter.post('/posts', postController.store)
+    parentRouter.get('/posts/:id/edit', postController.edit)
+    parentRouter.put('/posts/:id', postController.update)
+    parentRouter.delete('/posts/:id', postController.destroy)
 }
 export default initializeEndpoints;
